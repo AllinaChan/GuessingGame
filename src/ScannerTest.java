7f6f24c747c751cba4014aab5ff9f312a6337802
10760 <zchen5143@bths.edu>
2018-10-09 17:14:10 -0400

    Initial commit

---
+++ src/ScannerTest.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        Scanner name = new Scanner (System.in);
+        System.out.println("Hello user! What is your name?");
+        String userName= name.nextLine();
+        System.out.println("Hello "+ userName+ ", nice to meet you!");
+    }
+}
+
